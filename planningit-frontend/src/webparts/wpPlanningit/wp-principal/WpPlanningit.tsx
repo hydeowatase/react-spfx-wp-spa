5f050fca4363013937ff4cf1246f4e26a2875777
hydeowatase <hydeowatase@gmail.com>
2021-03-11 08:01:28 -0300

    PlanningIT: Menu Hover.

--- planningit-frontend/src/webparts/wpPlanningit/wp-principal/WpPlanningit.tsx
+++ planningit-frontend/src/webparts/wpPlanningit/wp-principal/WpPlanningit.tsx
@@ -1,70 +1,91 @@
-import * as React from 'react';
-import { IWpPlanningitProps } from './IWpPlanningitProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import { IWpPlanningitProps } from "./IWpPlanningitProps";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-import { BrowserRouter as Router, Link, Switch, Route, NavLink, Redirect, HashRouter } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route,
+  NavLink,
+  Redirect,
+  HashRouter,
+} from "react-router-dom";
 
-import Mainstyles from './WpPlanningit.module.scss';
-import { MenuContainer } from './Menustyles';
+import Mainstyles from "./WpPlanningit.module.scss";
+import Menustyles from "./Menustyles.module.scss";
 
-
-import Menu from './components/Menu/Menu';
-import BINewForm from './components/BINewForm/BINewForm';
-import Consulta from './components/Search/Search';
-import BIDetails from './components/BIDetails/BIDetails';
-import RealocarBudget from './components/ReallocateBudget/ReallocateBudget';
-import Scenario from './components/Scenario/Scenario';
-import CreateScenario from './components/CreateScenario/CreateScenario';
-import UploadFiles from './components/UploadFiles/UploadFiles';
+import Menu from "./components/Menu/Menu";
+import BINewForm from "./components/BINewForm/BINewForm";
+import Consulta from "./components/Search/Search";
+import BIDetails from "./components/BIDetails/BIDetails";
+import RealocarBudget from "./components/ReallocateBudget/ReallocateBudget";
+import Scenario from "./components/Scenario/Scenario";
+import CreateScenario from "./components/CreateScenario/CreateScenario";
+import UploadFiles from "./components/UploadFiles/UploadFiles";
 
 const WpPlanningit: React.FC<IWpPlanningitProps> = (props) => {
-  const listBaseUrl = `${props.context.pageContext.web.absoluteUrl}/${props.listIconUrl}`.replace(/\s/g, "");
-  const rootUrl = "/sites/jh-dev-planningit/SitePages/Teste.aspx?debug=true&noredir=true&debugManifestsFile=https://localhost:4321/temp/manifests.js";
+  const listBaseUrl = `${props.context.pageContext.web.absoluteUrl}/${props.listIconUrl}`.replace(
+    /\s/g,
+    ""
+  );
+  const rootUrl =
+    "/sites/jh-dev-planningit/SitePages/Teste.aspx?debug=true&noredir=true&debugManifestsFile=https://localhost:4321/temp/manifests.js";
 
   return (
     <HashRouter>
       <div className={Mainstyles.wpPlanningitContainer}>
         <div className={Mainstyles.wpPlanningitContent}>
           {/* <Menu iconUrl={`${props.context.pageContext.web.absoluteUrl}/${props.listIconUrl}`.replace(/\s/g, "")} /> */}
-          <MenuContainer>
-            <ul>
-              <li>
-                <img src={`${listBaseUrl}/begin-icon.png`} />
-                <NavLink to="">Inicio</NavLink>
-              </li>
-              <li>
-                <img src={`${listBaseUrl}/add-bi-icon.png`} />
-                <NavLink to="/BINewForm">Novo BI</NavLink>
-              </li>
-              <li>
-                <img src={`${listBaseUrl}/search-bi-icon.png`} />
-                <NavLink to="/Consulta">Consulta</NavLink>
-              </li>
-              <li>
-                <img src={`${listBaseUrl}/scenario-icon.png`} />
-                <a>Cenários</a>
-              </li>
-              <li>
-                <img src={`${listBaseUrl}/update-scenario-icon.png`} />
-                <a>
-                  Atualizar Base
-                  </a>
-              </li>
-            </ul>
-          </MenuContainer>
+          <div className={Menustyles.MenuContainer}>
+            <nav className={Menustyles.navbar}>
+              <ul className={Menustyles.navbarnav} >
+                <li className={Menustyles.navbaritem}>
+                  <NavLink className={Menustyles.navlink} to="/">
+                    <img src={`${listBaseUrl}/begin-icon.png`} />
+                    <span className={Menustyles.linktext}>Inicio</span>
+                  </NavLink>
+                </li>
+                <li className={Menustyles.navbaritem}>
+                  <NavLink className={Menustyles.navlink} to="/BINewForm">
+                    <img src={`${listBaseUrl}/add-bi-icon.png`} />
+                    <span className={Menustyles.linktext}>Novo BI</span>
+                  </NavLink>
+                </li>
+                <li className={Menustyles.navbaritem}>
+                  <NavLink className={Menustyles.navlink} to="/Consulta">
+                    <img src={`${listBaseUrl}/search-bi-icon.png`} />
+                    <span className={Menustyles.linktext}>Consulta</span>
+                  </NavLink>
+                </li>
+                <li className={Menustyles.navbaritem}>
+                  <NavLink className={Menustyles.navlink} to="/Scenario">
+                    <img src={`${listBaseUrl}/scenario-icon.png`} />
+                    <span className={Menustyles.linktext}>Cenários</span>
+                  </NavLink>
+                </li>
+                <li className={Menustyles.navbaritem}>
+                  <NavLink className={Menustyles.navlink} to="/UploadFiles">
+                    <img src={`${listBaseUrl}/update-scenario-icon.png`} />
+                    <span className={Menustyles.linktext}>Atualizar Base</span>
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
           <div className={Mainstyles.mainArea}>
             <Switch>
-              <Route path="/" exact >
+              <Route path="/" exact>
                 {<div>home</div>}
               </Route>
               <Route path="/BINewForm" component={BINewForm} />
               <Route path="/Consulta" component={Consulta}></Route>
-              <Route path="/BIDetails" component={BIDetails} ></Route>
-              <Route path="/RealocarBudget" component={RealocarBudget} ></Route>
-              <Route path="/Scenario" component={Scenario} ></Route>
-              <Route path="/CreateScenario" component={CreateScenario} ></Route>
-              <Route path="/UploadFiles" component={UploadFiles} ></Route>
-              <Redirect path="*" to={'/'} />
+              <Route path="/BIDetails" component={BIDetails}></Route>
+              <Route path="/RealocarBudget" component={RealocarBudget}></Route>
+              <Route path="/Scenario" component={Scenario}></Route>
+              <Route path="/CreateScenario" component={CreateScenario}></Route>
+              <Route path="/UploadFiles" component={UploadFiles}></Route>
+              <Redirect path="*" to={"/"} />
             </Switch>
           </div>
         </div>
@@ -72,6 +93,5 @@ const WpPlanningit: React.FC<IWpPlanningitProps> = (props) => {
     </HashRouter>
   );
 };
-
 
 export default WpPlanningit;
